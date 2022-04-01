34fc7b40e553918fa1d13edfcb73f26b4cac4fe8
Group-5-Zen-Garden <dhokerlund@gmail.com>
2022-03-31 22:42:21 -0600

    fixed a few small bugs and added some comments

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => 
 {
-document.querySelector('body').setAttribute('class', 'no-animation-time')
-const menuIcon = document.createElement('div');
-const line1 = document.createElement('span');
-const line2 = document.createElement('span');
-const line3 = document.createElement('span');
-document.body.prepend(menuIcon);
-menuIcon.prepend(line3);
+document.querySelector('body').setAttribute('class', 'no-animation-time') //adds the no-animation class to the body
+const menuIcon = document.createElement('div'); //create a div for the menu icon
+const line1 = document.createElement('span'); //create the top bar
+const line2 = document.createElement('span'); //create the middle bar
+const line3 = document.createElement('span'); //create the bottom bar
+document.body.prepend(menuIcon); //add the menu-icon div to the top of the body
+menuIcon.prepend(line3); //adds the lines inside to top of div
 menuIcon.prepend(line2);
 menuIcon.prepend(line1);
-menuIcon.setAttribute('class', 'menu-icon');
-menuIcon.setAttribute('onclick', 'toggleNav()');
-line1.setAttribute('class', 'line1');
-line2.setAttribute('class', 'line2');
-line3.setAttribute('class', 'line3');
+menuIcon.setAttribute('class', 'menu-icon'); //assigns a class to div
+menuIcon.setAttribute('onclick', 'toggleNav()'); //gives a click attribute to the div
+line1.setAttribute('class', 'line1'); //assigns a class to line1
+line2.setAttribute('class', 'line2'); //assigns a class to line2
+line3.setAttribute('class', 'line3'); //assigns a class to line3
 
 const updateSidebar = document.querySelector('.sidebar'); //selects the left-positioned sidebar
 const updateMenuIcon = document.querySelector('.menu-icon'); //selects the left-positioned menu
@@ -30,7 +30,9 @@ toggleNav = () =>
 	updateLine3.classList.toggle('rightX'); //toggles to right-positioned rules for third line
 }
 
-setTimeout(function allowAnimations()
+/*on page load, after 600ms, toggles the "all-in-body" rule from no animation time to a specified animation
+time set in an opposing "all-in-body" rule. By doing so this prevents animations being played when page is loaded.*/
+setTimeout(allowAnimations = () =>
 {
 	document.body.classList.toggle('standard-animation-time');
 },  600);
